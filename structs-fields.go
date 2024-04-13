b0ae17b97a453137df62b3625313cffbdddd8e8c
matheuscascao <matheusfranco23@hotmail.com>
2024-04-13 20:18:35 -0300

    .

--- structs-fields.go
+++ structs-fields.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
 
-func main() {
-	v := Vertex{1, 2}
-	p := &v
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+)
 
-	p.X = 999
-	fmt.Println(v)
-	fmt.Println(&p)
+func main() {
+	fmt.Println(v1, *p, v2, v3)
 }
