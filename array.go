a9fae576f23675186fa12d37cbb5ac3bba7b7bc0
matheuscascao <matheusfranco23@hotmail.com>
2024-04-14 17:39:20 -0300

    ,

--- array.go
+++ array.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "aa"
-	a[1] = "bb"
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
 
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
